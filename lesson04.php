cc8c8354c4285b24404e87d8a39d50ad498d7479
AGE1997 <baseball.0331e.t@gmail.com>
2022-06-23 18:36:19 +0900

    Initial commit

---
+++ lesson04.php
@@ -0,0 +1,12 @@
+﻿<?php
+// 連想配列の要素name, age, genderに
+// 山田,20,女性
+// という値を格納し、
+
+// 山田
+// 20歳・女性
+
+// という形で出力してください。
+$women = array("name" => "山田", "age" => 20, "gender" => "女性");
+echo $women["name"]."\n";
+echo $women["age"]."歳"."・".$women["gender"];
---
+++ lesson11.php
@@ -0,0 +1,27 @@
+<?php
+// 以下をそれぞれ表示してください。
+// 表示するタイミングによって自動で算出すること
+// ・現在日時（yyyy年mm月dd日 H時i分s秒）
+// ・現在日時から３日後
+// ・現在日時から１２時間前
+// ・2020年元旦から現在までの経過日数
+
+// 日時がおかしい場合、PHPのタイムゾーン設定を行ってください
+
+date_default_timezone_set('Asia/Tokyo');
+
+echo "現在日時 (".date('Y年m月d日 H時i分s秒').")";
+echo "\n";
+
+echo "現在日時から3日後 ".date('Y年m月d日 H時i分s秒', strtotime("+3 day"));
+echo "\n";
+
+echo "現在日時から12時間前 ".date('Y年m月d日 H時i分s秒', strtotime("-12 hour"));
+echo "\n";
+
+$today = new DateTime('now');
+$date_time = new DateTime('2020-01-01');
+ 
+$diff = $today->diff($date_time);
+
+echo "2020年元旦から現在までの経過日数は".$diff->days."日です。";
